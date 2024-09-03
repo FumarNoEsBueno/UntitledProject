2cf9043beadb7d33af0d1089d2f4323bad7d84b3
Mercelo <marcelo.murillo.99@hotmail.com>
2024-09-02 23:26:25 -0400

    cambios

--- Escenas/Systema.cs
+++ Escenas/Systema.cs
@@ -4,16 +4,16 @@ using System;
 public partial class Systema : Node2D
 {
 	Mapa mapa = new Mapa();
-    EstadoSystema estado;
+	EstadoSystema estado;
 
 	public override void _Ready()
 	{
-        this.estado = new JugadorEnMazmorra(this);
+		this.estado = new JugadorEnMazmorra(this);
 		this.AddChild(mapa);
 	}
 
 	public override void _Process(double delta)
 	{
-        this.estado.comportamiento();
+		this.estado.comportamiento();
 	}
 }
